5b542501ae528cb62c597a75d2eaec3671526664
shaikhfarheen11 <farheens554>
2023-09-18 20:16:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useCallback } from 'react';
-
 import MoviesList from './Component/Movies/MoviesList';
 import AddMovie from './Component/Movies/AddMovie';
 import './App.css';
@@ -9,29 +8,55 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
+  const deleteMovieHandler = async (id) => {
+    try {
+      const response = await fetch(
+        `https://react-hp-325a3-default-rtdb.firebaseio.com/movies/${id}.json`,
+        {
+          method: 'DELETE',
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Something went wrong while deleting the movie.');
+      }
+
+      // Update the UI by removing the deleted movie from the movies state
+      setMovies((prevMovies) => prevMovies.filter((movie) => movie.id !== id));
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
   const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
+
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch(
+        'https://react-hp-325a3-default-rtdb.firebaseio.com/movies.json'
+      );
+
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          ...data[key], // Spread the movie data
+        });
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
+
     setIsLoading(false);
   }, []);
 
@@ -39,36 +64,52 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
-  }
+  const addMovieHandler = async (movie) => {
+    try {
+      const response = await fetch(
+        'https://react-hp-325a3-default-rtdb.firebaseio.com/movies.json',
+        {
+          method: 'POST',
+          body: JSON.stringify(movie),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
 
-  let content = <p>Found no movies.</p>;
+      if (!response.ok) {
+        throw new Error('Failed to add the movie.');
+      }
 
-  if (movies.length > 0) {
-    content = <MoviesList movies={movies} />;
-  }
+      const data = await response.json();
 
-  if (error) {
-    content = <p>{error}</p>;
-  }
-
-  if (isLoading) {
-    content = <p>Loading...</p>;
-  }
+      // Update the UI by adding the new movie to the movies state
+      setMovies((prevMovies) => [
+        ...prevMovies,
+        {
+          id: data.name,
+          ...movie, // Spread the movie data
+        },
+      ]);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
 
   return (
-    <React.Fragment>
-        <html className='background'>
+    <div className='background'>
       <section>
         <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>{content}</section>
-      </html>
-    </React.Fragment>
+      <section>
+        <MoviesList movies={movies} onDeleteMovie={deleteMovieHandler} />
+      </section>
+      {error && <p>{error}</p>}
+      {isLoading && <p>Loading...</p>}
+    </div>
   );
 }
 
--- src/Component/Movies/MoviesList.js
+++ src/Component/Movies/MoviesList.js
@@ -1,18 +1,23 @@
 import React from 'react';
-
 import Movie from './Movie';
 import classes from './MoviesList.module.css';
 
 const MoviesList = (props) => {
+  const deleteMovieHandler = (id) => {
+    props.onDeleteMovie(id); // Pass the id to the parent component for deletion
+  };
+
   return (
     <ul className={classes['movies-list']}>
       {props.movies.map((movie) => (
-        <Movie
-          key={movie.id}
-          title={movie.title}
-          releaseDate={movie.releaseDate}
-          openingText={movie.openingText}
-        />
+        <li key={movie.id}>
+          <Movie
+            title={movie.title}
+            releaseDate={movie.releaseDate}
+            openingText={movie.openingText}
+          />
+          <button onClick={() => deleteMovieHandler(movie.id)}>Delete</button>
+        </li>
       ))}
     </ul>
   );
